0f1924b229630f3bfe79dcf68d5eadd2620b309d
0xnofoot <jnwang99@outlook.com>
2022-12-03 20:04:46 +0800

    12-03

--- proxy-set.sh
+++ proxy-set.sh
@@ -2,19 +2,19 @@
 # this sh is for set the proxy and unset
 
 if [ "$1" = "start" ]; then
-	export https_proxy=127.0.0.1:7890 
-	export http_proxy=127.0.0.1:7890 
+	export https_proxy=127.0.0.1:7890
+	export http_proxy=127.0.0.1:7890
 	export socks_proxy=127.0.0.1:7891
 elif [ "$1" = "t" ]; then
-	export https_proxy=127.0.0.1:7890 
-	export http_proxy=127.0.0.1:7890 
+	export https_proxy=127.0.0.1:7890
+	export http_proxy=127.0.0.1:7890
 	export socks_proxy=127.0.0.1:7891
 elif [ "$1" = "stop" ]; then
-	unset https_proxy 
+	unset https_proxy
 	unset http_proxy
 	unset socks_proxy
 elif [ "$1" = "p" ]; then
-	unset https_proxy 
+	unset https_proxy
 	unset http_proxy
 	unset socks_proxy
 fi
